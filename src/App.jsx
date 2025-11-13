f0314604cdad6b27c645b9f760d834a939b7a523
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-11-13 19:54:34 +0100

    made the button call to the PokemonApplication component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ function App() {
   return (
     <>
       <h1>Pokemon Application</h1>
-      <button onClick={() => {setShow(!show)}}>Start Pokemon App</button>
+      <button onClick={() => {setShow(true)}}>Start Pokemon App</button>
+      {show && 
+        <ul>
+          {pokemons.map((pokemon, id) => <PokemonApplication data = {pokemon} key = {id}/>)}
+        </ul>
+      }
     </>
   )
 }
