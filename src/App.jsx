00fbbaa061f1b322a060a063057858c6e7f3c682
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-11-13 20:10:13 +0100

    made an array to save the pokemon in, made in get information when the PokemonApplication is rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import PokemonApplication from './components/PokemonApplication'
 
 function App() {
 
   const [show, setShow] = useState(false)
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect (() => {
+    if (!show) return
+    const getPokemon = async () =>{
+      let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      let json = await response.json()
+
+      console.log("HEJ")
+      setPokemons(json.results)
+    }
+
+    getPokemon()
+  }, [show])
 
   return (
     <>
