e0069a4f539910e19dc631a1a907b7e8c6f42897
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-11-18 14:48:46 +0100

    relocated the useEffect from App to PokemonApplication

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,12 @@ function App() {
 
   const [show, setShow] = useState(false)
 
-  const [pokemons, setPokemons] = useState([])
-
-  useEffect (() => {
-    if (!show) return
-    const getPokemon = async () =>{
-      let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      let json = await response.json()
-
-      console.log("HEJ")
-      setPokemons(json.results)
-    }
-
-    getPokemon()
-  }, [show])
-
   return (
     <>
       <h1>Pokemon Application</h1>
       <button onClick={() => {setShow(true)}}>Start Pokemon App</button>
       {show && 
-        <ul>
-          {pokemons.map((pokemon, id) => <PokemonApplication data = {pokemon} key = {id}/>)}
-        </ul>
+        <PokemonApplication/>
       }
     </>
   )
--- src/components/PokemonApplication.jsx
+++ src/components/PokemonApplication.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react'
+import Pokemon from './Pokemon'
+
 const PokemonApplication = () => {
 
+    const [currentPokemon, setCurrentPokemon] = useState("")
+    const [showDetails, setShowDetails] = useState(false)
+    const [pokemons, setPokemons] = useState([])
+    useEffect (() => {
+      const getPokemon = async () =>{
+        let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        let json = await response.json()
+
+        setPokemons(json.results)
+    }
+
+    getPokemon()
+  }, [])
+
+  return(
+    <div>
+      <select onChange={(e) => {setCurrentPokemon(e.target.value)}}>
+        {pokemons.map((pokemon, id) => <option key={id} value={pokemon.url}>{pokemon.name}</option>)}
+      </select>
+      <button onClick={() => {setShowDetails(true)}}>Mer info</button>
+      {showDetails && 
+      <Pokemon link={currentPokemon}/>}
+    </div>
+  )
 }
 
 export default PokemonApplication
