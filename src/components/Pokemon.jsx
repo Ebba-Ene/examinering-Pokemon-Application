69f4c55a94fb797f1af261d0706e87aea73c3f51
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-11-20 11:36:14 +0100

    fetch the info

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,5 +1,33 @@
-const Pokemon = () => {
+import { useEffect, useState } from "react"
 
+const Pokemon = (props) => {
+
+  const [pokemonInfo, setPokemonInfo] = useState(null)
+
+  console.log(props.link)
+
+    useEffect (() => {
+      const getPokemonInfo = async () =>{
+        let response = await fetch(props.link)
+        let json = await response.json()
+
+        setPokemonInfo(json)
+      }
+
+    getPokemonInfo()
+  }, [props.link])
+
+  return(
+    <div>
+      {!pokemonInfo && <p>Loading...</p>}
+      {pokemonInfo && 
+        <div>
+          <h1>{pokemonInfo.name}</h1>
+          <p>Base experience: {pokemonInfo.base_experience}</p>
+        </div>
+      }
+    </div>
+  )
 }
 
 export default Pokemon
